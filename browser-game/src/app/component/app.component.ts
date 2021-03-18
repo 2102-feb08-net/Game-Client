21c257935381d9b5e9461bce6ef7d978b13905e3
Cole Samuelsen <csamuelsen@wisc.edu>
2021-03-18 13:26:45 -0500

    shell of game

---
+++ browser-game/src/app/component/app.component.ts
@@ -0,0 +1,36 @@
+import { Component ,OnInit, ViewChild, ElementRef, HostListener} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  
+  title = 'browser-game';
+
+  keyPress(event: KeyboardEvent) {
+    console.log(event)
+}
+
+  @ViewChild("canvas")
+  private gameCanvas: ElementRef | undefined;
+
+  private context: any;
+
+  public ngOnInit(){
+
+  }
+
+  public ngAfterViewInit(){
+
+    this.context = this.gameCanvas?.nativeElement.getContext("2d");
+
+    this.context.fillRect(20,20,20,20);
+  }
+
+  @HostListener('document:keydown', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+		this.keyPress(event);
+	}
+
+}
