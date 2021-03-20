088f874e3549bc1596946fe2bd7f338ad0f04a38
drincon2 <diegorima98@hotmail.com>
2021-03-20 13:44:51 -0600

    Added Tileset folder with Tile.png to draw tiles to map. Replaced background with tile from Tile.png.

--- browser-game/src/app/services/backgroundservice.ts
+++ browser-game/src/app/services/backgroundservice.ts
@@ -8,6 +8,9 @@ export class BackgroundService {
         this.image.height = this.imgheight;
         this.image.width = this.imgwidth;
         this.image.src = this.map.imgsrc;
+        this.tile.height = 16;
+        this.tile.width = 16;
+        this.tile.src = "assets/imgs/Tileset/Tile.png";
     }
 
     map: BackgroundMap = {
@@ -16,14 +19,29 @@ export class BackgroundService {
         width: 400
     };
 
-    imgheight = 300;
-    imgwidth = 300;
+    imgheight = 320;
+    imgwidth = 320;
 
     image: HTMLImageElement = new Image();
+    
+    tilex = 0;
+    tiley= 0;
+
+    tile: HTMLImageElement = new Image();
+
+
+    // Generate Map
+    
+    // Sync layers
 
     //Load map
     loadMapContext(mapContext: any){
-        mapContext.drawImage(this.image, 0, 0, this.map.width, this.map.height);
+        for (let i = 0; i < this.imgheight; i +=16) {
+            for(let j =0; j < this.imgwidth; j+=16) {
+                mapContext.drawImage(this.tile, 0, 0, 16, 16, this.tilex+j, this.tiley+i, this.tile.width, this.tile.height);
+            }
+        }
+        
     }
     //Handle map events (loading map zones or other maps)
 
